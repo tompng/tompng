466bac3f43ff65d521cd831b1992f735e0129dbc
tompng <tomoyapenguin@gmail.com>
2019-03-03 00:31:13 +0900

    rm method

--- main.rb
+++ main.rb
@@ -61,70 +61,60 @@ curves = curves_base.zip(starts).map do |numbers, (x, y)|
   end
 end
 
-def show(canvas)
-  lines = (canvas.size / 2).times.map do |y|
-    canvas.size.times.map do |x|
-      a = canvas[x][2 * y]
-      b = canvas[x][2 * y + 1]
-      ai = (a * 3.99).floor
-      bi = (b * 3.99).floor
-      ai = 1 if a > 0 && ai == 0
-      bi = 1 if b > 0 && bi == 0
-      [%( '"^),%(.:TY),%(,;EP),%(xLJ#)][bi][ai]
-    end.join.gsub(/ +$/, '')
-  end
-  $><< "\e[1;1H\e[2J#{lines.join("\n")}"
-end
-
-
 require 'io/console'
-def render(curves, colors, size)
-  rows, cols = IO.console.winsize
-  size = [rows * 2, cols].min
-  canvas = Array.new(size) { [0] * size }
-  curves.zip(colors).each do |curve, color|
-    coords = []
-    curve.each do |x, y, dx1, dy1, dx2, dy2, dx3, dy3|
-      dx=dx1+dx2+dx3
-      dy=dy1+dy2+dy3
-      step = [[dx.abs, dy.abs].max * size / 1024 / 2, 1].max
-      step.times do |i|
-        t = i.fdiv step
-        a = 3 * t * (1 - t) * (1 - t)
-        b = 3 * t * t * (1 - t)
-        c = t * t * t
-        px = (x + a * dx1 + b * (dx1+dx2) + c * dx) * size / 1024
-        py = (y + a * dy1 + b * (dy1+dy2) + c * dy) * size / 1024
-        coords.push [px, py]
-      end
-    end
-    prev_x, prev_y = coords.last
-    xs = coords.map(&:first)
-    segments = {}
-    coords.zip(xs.rotate(-1), xs.rotate(1)).each do |(next_x, next_y), xa, xb|
-      x0, x1 = prev_x < next_x ? [prev_x.ceil, next_x.floor] : [next_x.ceil, prev_x.floor]
-      (x0..x1).each do |ix|
-        next if ix == prev_x
-        next if ix == next_x && (xa - next_x) * (xb - next_x) >= 0
-        y = prev_y + (next_y - prev_y) * (ix - prev_x) / ((next_x - prev_x).nonzero? || 1)
-        (segments[ix] ||= []) << y
-      end
-      prev_x = next_x
-      prev_y = next_y
-    end
-    segments.each do |ix, ys|
-      break unless canvas[ix]
-      ys.sort.each_slice(2) do |y1, y2|
-        break if y2.nil?
-        (y1.ceil...y2).each do |iy|
-          canvas[ix][iy] = color if 0 <= iy && iy < size
-        end
-      end
-    end
-  end
-  show(canvas)
-end
 
 rows, cols = IO.console.winsize
 size = [rows * 2, cols].min
-render curves, colors, size
+canvas = Array.new(size) { [0] * size }
+curves.zip(colors).each do |curve, color|
+  coords = []
+  curve.each do |x, y, dx1, dy1, dx2, dy2, dx3, dy3|
+    dx=dx1+dx2+dx3
+    dy=dy1+dy2+dy3
+    step = [[dx.abs, dy.abs].max * size / 1024 / 2, 1].max
+    step.times do |i|
+      t = i.fdiv step
+      a = 3 * t * (1 - t) * (1 - t)
+      b = 3 * t * t * (1 - t)
+      c = t * t * t
+      px = (x + a * dx1 + b * (dx1+dx2) + c * dx) * size / 1024
+      py = (y + a * dy1 + b * (dy1+dy2) + c * dy) * size / 1024
+      coords.push [px, py]
+    end
+  end
+  prev_x, prev_y = coords.last
+  xs = coords.map(&:first)
+  segments = {}
+  coords.zip(xs.rotate(-1), xs.rotate(1)).each do |(next_x, next_y), xa, xb|
+    x0, x1 = prev_x < next_x ? [prev_x.ceil, next_x.floor] : [next_x.ceil, prev_x.floor]
+    (x0..x1).each do |ix|
+      next if ix == prev_x
+      next if ix == next_x && (xa - next_x) * (xb - next_x) >= 0
+      y = prev_y + (next_y - prev_y) * (ix - prev_x) / ((next_x - prev_x).nonzero? || 1)
+      (segments[ix] ||= []) << y
+    end
+    prev_x = next_x
+    prev_y = next_y
+  end
+  segments.each do |ix, ys|
+    break unless canvas[ix]
+    ys.sort.each_slice(2) do |y1, y2|
+      break if y2.nil?
+      (y1.ceil...y2).each do |iy|
+        canvas[ix][iy] = color if 0 <= iy && iy < size
+      end
+    end
+  end
+end
+
+(size / 2).times do |y|
+  puts size.times.map{|x|
+    a = canvas[x][2 * y]
+    b = canvas[x][2 * y + 1]
+    ai = (a * 3.99).floor
+    bi = (b * 3.99).floor
+    ai = 1 if a > 0 && ai == 0
+    bi = 1 if b > 0 && bi == 0
+    [%( '"^),%(.:TY),%(,;EP),%(xLJ#)][bi][ai]
+  }.join.gsub(/ +$/, '')
+end
