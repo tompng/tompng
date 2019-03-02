5add510c5e624f3f880718ef507be0f5ca0c08af
tompng <tomoyapenguin@gmail.com>
2019-03-03 06:25:11 +0900

    tompng.rb

---
+++ tompng.rb
@@ -0,0 +1,26 @@
+require'io/console';h,w=IO.console&.winsize||[0,0];g=(0...m=[[h,w/2].min*2,48].max).map{[0]*m};[[524,489,%w(H9zR0b8H/R4EJlTy5fyPUDVdSVeyIiuzaChOoHiIwhiK9wfZpEuoHKGEBkHQcBv9EWmH28FW
+XMf0kHDYhi0R3AYdgxEYgYNDGZiGKZ3zOTeC52RWRdv5Zr414suo7ujuEpZHeZyNUsMMEE5a0e41OvXu5/b3wvf/z299+f/lX6H4wQY)*l=''],[509,22,%w(FVAgpVA59DvdSraTKmVyJoeGk6FkaGhSNB1JDwE0SZK1gWKgHNjP98f1x9
+a2uGluWnANC7Igx8wwS1+Tv+Qv/EiKpBD6iiAGPoFiME5G04EkHxpGEyxBOYmQiI2wMA1HEjEYg8Pq0Ua0jt8ggYR3yGADHbfcjOnkTc4M4RmZFpFWop6oD/wT+h/6D76Doi+pv67velZAD31yUDATzJBJ4WGhoZERsVwsmx8ZtCGHv7iCL3
+b/9H73/rR/1n82fn170yv3y7Tbr/cb9Xq5Wqo9rxQrxXL7T7PZbPQ6DVHvV0wZBA)*l],[628,111,%w(APz70G6321JUVFU/w/+DhWA13My2cnqsHmvGssVuuBthciZt0/0oiMF4cDCDDMT+E4GFNqRP+0N/R0VURHQIhkEqDgAArBnqBDv
+j3/MO7wJBNDHE      U8tsBFBJBfmvOqpT7tV1Xf/0Xdqh3QgMgdDIJGSQaWawRlIppHE/4ailkFpooe5rYIAhhhrqsxop3LdaaeUbsA7r9HuoEqpEa8lmspVr5EzODOPpYCo4aDgAAQ)*l],[639,115,%w(EMzODowPTIweFIlH4zSNKG
+KYeOihMsoo06y      3ZUfUZNu3XZF74AEEHnjQLOqe7jth+65vasBDDwD0AQ)*l],[417,358,%w(DndGi+FieHu5nqQmu6lwOBxOJ2KCCULYYUc/4P/4H/pACrjA/pI+6TOBIQImCcIwApMkQsIszhGHUUn7tK++gC7seoc7uJv/Bhqw0
+a44bbX10EGXYT      EQhymYU1k5/Veyl+xREVERFZEhGIbhMCUUA0oQhiGNIIIeAwyIjeEgDobCjDLGHeWUw6kYIEAkklCaouu6nukBAzQyCCCQHhnKh3LB0cHBwcHB4eFMOJMbAgAA1U+yLuq9/0ADDSwCCGSHAgCA/NAviGLje6fUKdW
+LqqM6so0                88rFIMBgMZ         oYhD4Humb7p      V78       YYQSQ1jt       ngUUG9kOW      Ogo         ppJBy5qmni      mukU       ccbbXWoZvu2L3t9I       0wLt      mmbVjPNc
+CPcCjbCz                XA9Wg/                 Wg6VgG7                   V              Rxwsjj                     Olqq50B      B              hjomAEGi                     J4DFgBoc
+Q)*l],[9                +733      ,435,%w(       HkcZp         JAoC           imgL       EQght          Rhi60        X2CAT          dMggA       6Sz1gp       voYWAW         mhBp2+AA
+RYDFA)*l],[33      +400,624     ,%w(AITHhrKh      THZS       iIUYBSEf       8sl+UAd      1SALn        nfUGGNiJCt      RHfd        zTXV0rdm      udWu0      NAAE)*l],[       673,622,
+%w(AOTaMRWXQz      vcBm0QAA     +cshpo0GA90iV     d2ud      93kgBEA)*      l],[513,3     +800,      %w(APzLi3g/rl     ImZZ      JmIIyjOMtgj     DEE1      BOfasRBHIwZz      BBDEBGHH
+flCO6zDu9RQzR      DWyNi2FU     54KYUSbdEUrf6      7Mg      AB)*l],[9      +680,683,     %w(7W      h9OQiBIKIEURS      RHH      LIWJAFaTQRJ     BxTx     C03kT9cBRVzHH      AfYdQTgD
+1yyAHltTfeSin      FhVs1W7V     90okG)*l],[9+     470,      766,%w(Do      ORMARDhAL     qqWUA      AJBAERAGORd2Y      R+z      AACgpdVOmbb     t275      qeesN9M4716J      92Ac1a6E
+BHngc)*l],[9+      650,765,     %w(ANAulVTFe      kxyi      RTXTAdVCI      VgOBvmERb     DYRzB       WQwAAEZZbXS      v2ev      2u/WmErJvrB     VO4L      rXTnurnfbFU       Ae3cY1op
+JADDjnsMCCQBB      nmhJMY4JB      74rhlvZAL      uiCLQ      woYYAAE)*      l],[600,2     +7*10        ,%w(ANTKb       dmSd      VUGZfAxVIlU     I+1UP       96P27BN         2RQZ6AeC
+vJqJEEZIUkB8l           FFMMW       YEE         gAtc8x      N6BBPHPIA      QvA9I7jkD     uugCv                       Yyhlt      mqcceeQwhBL     F2UIVU                      TIdNWHMY
+kVE5pBRpRVqgHu           6Gu3nJ               FVfUQgml      bIh+X1Rk2      7VcDfVxn0     jusCM      Q             IYLp0CTE      kBjkkEPWW2y     xjymmWItJJ         JG0      GhnkmSGG
+yLCEEvW9VkrZIqyhavw  7b/Em0cFOsB0pxP     rRXtzFXcJmQBwn8KA4AAE)*l],[496,205,%w(FkpmiSWGQQgR99h      jRxyy2BLlHXDGWRA)*l],[375,340,%w(DqiYCZtIbyCIwmg4hSGG0GGLbeQn6ZIu      bgAAQCdtn
+PX4j7feeOtB)*l],[645,130,%w(AJA8D/NwPBjncT4IxVmcJYKMMEIZ8BC0S33R77dkx3R0FWmrjVXQQZuj0AJb6HR0V5      c1AAE)*l],[433,379,%w(DlrosMMo7aIuIqM6oZO9jB6kB5lZ     IOmTIIkh       gthigzQqox
+ZsggpuozZpY4klNVAj7QS00AILPQQEQgyRZT7ouU2iCIzQFCGEEkQ89ayLFZFIAgmlllBAEWwTQSR1uI/65oeRRkprvfUO      eOQgCjvkkAXWWSeKzjnngQMB)*l],[843,387,%w(ANR+9tv9V                   qvknPXeO+gh
+isAQCsIwi7PEyHkIYwIRAtBD5a231jvjtO2avul3/gMAwMAhGGGEEPQB)*l]].map{|x,y,c|c=c.unpack('m');c=c[0      ].unpack('b*')[0].chars;[]while'1'!=c.pop;n=[];n<<(c             .shift==?0?(v=(
+c.shift(4)*l).to_i 2)>7?v-=16:v :(v=(c.shift(5+2*a=c.shift.to_i)*l).to_i 2)>15+a*48?v-40-a*112:v+7+a*15)while(c[0]);c=[];n.each_slice(6).map{|q,r,s,t,u,v|u+=s+=q;v+=t+=r;n=1+(u.abs
++v.abs)*(w=m/1024.0);n.to_i.times{|i|a=3*(1-z=i/n)**2*z;c<<[(x+a*q+z**3*u+s*b=3*z*z*(1-z))*w,(y+a*r+z**3*v+t*b)*w]};x+=u;y+=v};c}.zip([1,2,*([3]*8),4,4,*([5]*6)]){|c,a|s={};(0...(c
+.size)).map{|i|u,v=c[i-2];x,y=c[i-1];x0,x1=[u,x].sort;(x0.ceil..x1.floor).each{|j|j!=u&&(j!=x||(u-x)*(c[i][0]-x)<0)&&(s[j]||=[])<<v+(y-v)*(j-u)/(x-u)}};s.each{|i,t|t.sort_by(&:to_f
+).each_slice(2){|u,v|(u.ceil...v).each{|j|g[j][i]=a}}}};puts(g.each_slice(2).map{|a,b|(a.zip(b).map{|i,j|%(#{32.chr}`''"^.:]TYY,;IEPPcjL8RRxLJ&WWxLJ&##)[i+6*j]}*l).gsub(/\s+$/,l)})
